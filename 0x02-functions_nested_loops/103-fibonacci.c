cfa63c49924c1fe6effa9abca08f8fc106f8643e
chioma maryjane ezeibekwe <genevieveezeh786@gmail.com>
2022-09-13 16:12:28 -0700

    Task 15

--- 0x02-functions_nested_loops/103-fibonacci.c
+++ 0x02-functions_nested_loops/103-fibonacci.c
@@ -3,8 +3,8 @@
  * main - main block
  * Description: print the sum of even Fibonaaci numbers up to a fib value
  * not exceeding 4,000,000.
- * Return: 0
- */
+  * Return: 0
+  */
 int main(void)
 {
 	int i = 1, j = 2, total = 0;
