e7a7aa70dd19d763aaf6eb8c2600074361170b5e
chioma maryjane ezeibekwe <genevieveezeh786@gmail.com>
2022-09-15 16:20:48 -0700

    0-isupper.c

--- 0x04-more_functions_nested_loops/0-isupper.c
+++ 0x04-more_functions_nested_loops/0-isupper.c
@@ -1,11 +1,15 @@
 #include "main.h"
 /**
- * _isupper - check for upper case letter
+ * _isupper - checks if a character is uppercase or not
  * @c : character to check the case
  * Return:0 or 1
  */
 
 int _isupper(int c)
 {
-	return (c >= 65 && c <= 90);
+	if ((c >= 'A') && (c <= 'Z'))
+	{
+		return (1);
+	}
+	return (0);
 }
