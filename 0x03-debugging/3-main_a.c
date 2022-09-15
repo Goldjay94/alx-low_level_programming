6e3fdfb7684f40691d9fff2f39822fc5f515be41
chioma maryjane ezeibekwe <genevieveezeh786@gmail.com>
2022-09-15 13:55:20 -0700

    3-main_a.c

--- 0x03-debugging/3-main_a.c
+++ 0x03-debugging/3-main_a.c
@@ -1,10 +1,11 @@
 #include <stdio.h>
 #include "main.h"
 /**
- * main - takes a date and prints how many days are leftin the year, taking
+ * main - takes a date and prints how many days are left in the year, taking
  * leap years into account
  * Return: 0
  */
+
 int main(void)
 {
 	int month;
