e46b13e8b1c38d97fcc618e5c7619c1d2d7f71de
chioma maryjane ezeibekwe <genevieveezeh786@gmail.com>
2022-09-15 17:34:37 -0700

    1-isdigit.c

---
+++ 0x04-more_functions_nested_loops/1-isdigit.c
@@ -0,0 +1,12 @@
+#include "main.h"
+/**
+ * _isdigit - check for digits (0 through 9)
+ * @c: int type parameter
+ * Return: 1 if digit, else 0
+ */
+int _isdigit(int c)
+{
+	if (c > 47 && c < 58)
+		return (1);
+	return (0);
+}
