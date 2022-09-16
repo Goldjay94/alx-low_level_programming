caf3f88710de1a59b467cbe5b61247fbae3b2b9c
chioma maryjane ezeibekwe <genevieveezeh786@gmail.com>
2022-09-15 18:39:14 -0700

    0-isupper.c

--- 0x04-more_functions_nested_loops/0-isupper.c
+++ 0x04-more_functions_nested_loops/0-isupper.c
@@ -7,7 +7,7 @@
  */
 int _isupper(int c)
 {
-	if (c >= 64 && c <= 91)
+	if (c >= 'A' && c <= 'Z')
 	{
 		return (1);
 	}
@@ -15,5 +15,4 @@ int _isupper(int c)
 	{
 		return (0);
 	}
-	_putchar('\n');
 }
