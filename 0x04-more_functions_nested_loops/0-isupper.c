c966b62f6db46aef867f447887edface6bdbdc44
chioma maryjane ezeibekwe <genevieveezeh786@gmail.com>
2022-09-15 18:19:23 -0700

    0-isupper.c

--- 0x04-more_functions_nested_loops/0-isupper.c
+++ 0x04-more_functions_nested_loops/0-isupper.c
@@ -1,12 +1,13 @@
 #include "main.h"
+
 /**
  * _isupper - function that checks for uppercase characters
- * @c: int type number
+ * @c: int type parameter
  * Return: 1 if uppercase, else 0
  */
 int _isupper(int c)
 {
-	if (c > 65 && c < 90)
+	if (c > 64 && c < 91)
 	{
 		return (1);
 	}
--- 0x04-more_functions_nested_loops/main.h
+++ 0x04-more_functions_nested_loops/main.h
@@ -7,10 +7,6 @@
  * int prototypes (int)
  */
 
-int _putchar(char)
-int _isupper(int c);
-int _isdigit(int c);
-int mul(int a, int b);
 void print_numbers(void);
 void print_most_numbers(void);
 void more_numbers(void);
@@ -19,5 +15,10 @@ void print_diagonal(int n);
 void print_square(int size);
 void print_triangle(int size);
 void print_number(int n);
+int _putchar(char);
+int _isupper(int c);
+int _isdigit(int c);
+int mul(int a, int b);
 
 #endif /* MAIN_H */
+
