fc9cc8c3c6aab64205a6f4c28cb678e28a2ea71e
chioma maryjane ezeibekwe <genevieveezeh786@gmail.com>
2022-09-22 13:16:05 -0700

    103-infinite_add.c

--- 0x06-pointers_arrays_strings/main.h
+++ 0x06-pointers_arrays_strings/main.h
@@ -4,6 +4,7 @@
 #include <stdio.h>
 
 int _putchar(char c);
+int main(void);
 char *_strcat(char *dest, char *src);
 char *_strncat(char *dest, char *src, int n);
 char *_strncpy(char *dest, char *src, int n);
@@ -14,5 +15,7 @@ char *cap_string(char *);
 char *leet(char *);
 char *rot13(char *);
 void print_number(int n);
+char *infinite_add(char *n1, char *n2, char *r, int size_r);
+void print_buffer(char *b, int size);
 
 #endif /* MAIN_H */
