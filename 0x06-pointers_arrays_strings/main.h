8e34ecc67d2c1f15fe9327c00823127871b8e787
chioma maryjane ezeibekwe <genevieveezeh786@gmail.com>
2022-09-22 11:20:22 -0700

    102-magic.c

--- 0x06-pointers_arrays_strings/main.h
+++ 0x06-pointers_arrays_strings/main.h
@@ -4,6 +4,7 @@
 #include <stdio.h>
 
 int _putchar(char c);
+int main(void);
 char *_strcat(char *dest, char *src);
 char *_strncat(char *dest, char *src, int n);
 char *_strncpy(char *dest, char *src, int n);
