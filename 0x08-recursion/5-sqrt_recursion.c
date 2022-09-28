a119a147492be1f03c4cb8213747ca208ca7a98d
chioma maryjane ezeibekwe <genevieveezeh786@gmail.com>
2022-09-28 12:37:51 -0700

    5-sqrt_recursion.c

--- 0x08-recursion/5-sqrt_recursion.c
+++ 0x08-recursion/5-sqrt_recursion.c
@@ -1,36 +1,44 @@
 #include "main.h"
-
 /**
- * sqrt_recursion - returns the natural square root of a number
- *
- * @n: number
- * Return: int
+ * _evaluate - evaluate function sqrt
+ * @i: interger
+ * @n: interger
+ * Return: evaluate sqrt
  */
-int _sqrt_recursion(int n)
-{
-	int square = 2;
 
-	if (n < 0)
-		return (-1);
-	else if (n == 0 || n == 1)
+int _evaluate(int i, int n)
+{
+	/*Evaluate function*/
+	if (n == 0 || n == 1)
 		return (n);
-	return (is_sqrt(n, square));
+
+	else if (i * i < n)
+		return (_evaluate(i + 1, n));
+
+	else if (i * i == n) /*Condiction base*/
+		return (i);
+
+	return (-1);
+
+	return (-1);
 }
 
 /**
- * is_sqrt - function to check whether it's a natural square root or not
- *
- * @n: number
- * @square: test number
- * Return: int
+ * _sqrt_recursion - evaluate sqrt
+ * @n: interger
+ * Return: Sgrt_recursion
  */
-int is_sqrt(int n, int square)
+
+int _sqrt_recursion(int n)
 {
-	if (square * square == n)
-		return (square);
-	else if (square * square < n)
-		return (is_sqrt(n, square + 1));
-	else if (square * square > n)
+	int i = 0;
+
+	if (i < 0) /*If n is negative*/
 		return (-1);
-	return (-1);
+
+	else
+	{
+		return (_evaluate(i, n)); /*Recursive call*/
+	}
+
 }
