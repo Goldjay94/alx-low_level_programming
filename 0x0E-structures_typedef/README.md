01d7c1c1f6db4cde2fb183bc295de2f4d3c83bf3
chioma maryjane ezeibekwe <genevieveezeh786@gmail.com>
2022-10-10 10:46:55 -0700

    dog.h

--- 0x0E-structures_typedef/README.md
+++ 0x0E-structures_typedef/README.md
@@ -1,1 +1,1 @@
-0x0E-structures_typedef
+0x0E. C - Structures, typedef
--- 0x0E-structures_typedef/dog.h
+++ 0x0E-structures_typedef/dog.h
@@ -1,5 +1,5 @@
-#ifndef DOG_H
-#define DOG_H
+#ifndef _DOG_H_
+#define _DOG_H_
 
 /**
  * dog_t - Typedef for struct dog
@@ -27,4 +27,4 @@ void print_dog(struct dog *d);
 dog_t *new_dog(char *name, float age, char *owner);
 void free_dog(dog_t *d);
 
-#endif
+#endif /* DOG_H */
