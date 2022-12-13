28636e0fc13bfbe205d4d01e3e58ca1a57f56aad
chioma maryjane ezeibekwe <genevieveezeh786@gmail.com>
2022-12-12 23:30:16 -0800

    0x18-dynamic_libraries

---
+++ 0x18-dynamic_libraries/0-isupper.c
@@ -0,0 +1,18 @@
+#include "main.h"
+
+/**
+ * _isupper - function that checks for uppercase characters
+ * @c: int type parameter
+ * Return: 1 if uppercase, else 0
+ */
+int _isupper(int c)
+{
+	if (c >= 'A' && c <= 'Z')
+	{
+		return (1);
+	}
+	else
+	{
+		return (0);
+	}
+}
